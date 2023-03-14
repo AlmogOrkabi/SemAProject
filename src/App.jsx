0a5f02ef3e664537f17eb1b8e5a507a1414b9edf
AlmogOrkabi <117260151+AlmogOrkabi@users.noreply.github.com>
2023-03-14 17:58:33 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
       <Route path ='/login' element={<Login/>} />
       <Route path ='/register' element={<Register/>} />
       <Route path ='/profile' element={<Profile/>} >
-        <Route path='/profile/:id' element={<Profile/>}/>
+        <Route path='/profile/:username' element={<Profile/>}/>
       </Route>
       <Route path ='/admin' element={<Admin/>} />
       <Route path ='/edit' element={<Edit/>} />
